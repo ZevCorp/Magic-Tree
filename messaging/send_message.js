bd03312b1c6802762e77a287ea05729a618482ef
Felipe Maldonado <zevthefuture@gmail.com>
2025-12-04 22:34:09 -0500

    mejorando errores 3

--- messaging/send_message.js
+++ messaging/send_message.js
@@ -33,9 +33,6 @@ puppeteerConfig.args = [
     '--disable-setuid-sandbox',
     '--disable-dev-shm-usage',
     '--disable-accelerated-2d-canvas',
-    '--no-first-run',
-    '--no-zygote',
-    '--single-process',
     '--disable-gpu'
 ];
 
